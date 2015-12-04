c9952eb2c9103581b21dccb60dbdc40f6ab4aff2
bennyzimmer <benny274@msn.com>
2015-12-04 14:47:18 -0200

    atualizações sobre o arquivo Benny adicionadas

--- benny.php
+++ benny.php
@@ -1,1 +1,6 @@
-<h1>Benny Fodão ;)</h1>
+<center><h1>Informações</h1></center>
+<br>
+<p><b>Nome:</b> Benny Roger Zimmer</p>
+<p><b>Idade:</b> 19 Anos</p>
+<p><b>Nacionalidade:</b> Brasileiro</p>
+<p><b>Estudo:</b> Ensino superior (cursando)</p>
