c79a312d1f0c8efca606a8902994100746cff78a
nataliakt <natalia.kthiel@gmail.com>
2015-12-04 16:21:42 -0200

    Editando arquivo pessoal

--- index.php
+++ index.php
@@ -3,12 +3,39 @@
     <head>
         <meta charset="UTF-8">
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
         <title>GihHub</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+            <div class="page-header">
+                <h1>Técnico em Informática para Internet <small>Programação Web IV</small></h1>
+            </div>
+            <div class="jumbotron">
+                <div class="row">
+                    <a href="Andreza.php"
+                       class="btn btn-info btn-lg col-sm-6 col-sm-offset-3">Andreza</a>
+                </div>
+                <hr>
+                <div class="row">
+                    <a type="button" href="benny.php"
+                       class="btn btn-default btn-lg col-sm-6 col-sm-offset-3">Benny</a>
+                </div>
+                <hr>
+                <div class="row">
+                    <a type="button" href="julie.php"
+                       class="btn btn-warning btn-lg col-sm-6 col-sm-offset-3">Julie</a>
+                </div>
+                <hr>
+                <div class="row">
+                    <a type="button" href="junior.php"
+                       class="btn btn-success btn-lg col-sm-6 col-sm-offset-3">Junior</a>
+                </div>
+                <hr>
+                <div class="row">
+                    <a type="button" href="natalia.php"
+                       class="btn btn-danger btn-lg col-sm-6 col-sm-offset-3">Natalia</a>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
--- natalia.php
+++ natalia.php
@@ -1,8 +1,40 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <title>GihHub</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="page-header">
+                <h1>Informações <small>Natalia Kelim Thiel</small><a href="index.php" class="btn btn-default">Andreza</a></h1>
+            </div>
+            <div class="jumbotron">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Natalia Kelim Thiel</h3>
+                    </div>
+                    <div class="panel-body row">
+                        <div class="table-responsive col-sm-6 col-sm-offset-3">
+                            <table class="table table-hover table-bordered">
+                                <tr>
+                                    <td>Nacionalidade:</td>
+                                    <td>Brasileira</td>
+                                </tr>
+                                <tr>
+                                    <td>Nascimento:</td>
+                                    <td>15/12/1997</td>
+                                </tr>
+                                <tr>
+                                    <td>Educação:</td>
+                                    <td>Cursando Ensino Superior</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
