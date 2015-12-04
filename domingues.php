8c134277f6656139c7290c0405ff7512870d84f4
lucas_ds1 <lucas_ds1@10.1.105.14>
2015-12-04 16:01:50 -0200

    atualizado

--- domingues.php
+++ domingues.php
@@ -1,8 +1,14 @@
-<?php
+<h1>Lucas Domingues</h1>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<h3>Nome: </h3><h2>Lucas</h2>
+<br>
+<h3>Idade: </h3><h2>17 anos</h2>
+<br>
+<h3>Apelido: </h3><h2>Domingues</h2>
+<br>
+<h3>Escola: </h3><h2>E.E.B Tereza Cristina</h2>
+<br>
+<h3>Cidade: </h3><h2>Laurentino</h2>
+<br>
+<h3>Cidade Natal: </h3><h2>Curitiba</h2>
 
