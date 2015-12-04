a1984423f43c18526aefc1c6eb162674700178ec
nataliakt <natalia.kthiel@gmail.com>
2015-12-04 14:48:30 -0200

    Importando Bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
         <title>GihHub</title>
     </head>
     <body>
